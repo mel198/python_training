937d466f972b68febde3f83b324974df234acde2
Rom Vi <youutest@gmail.com>
2018-06-14 18:49:02 -0400

    Выделен вспомогателнный класс для работы с группами

---
+++ fixture/group.py
@@ -0,0 +1,34 @@
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def open_group_page(self):
+        wd = self.app.wd
+        # open group page
+        wd.find_element_by_link_text("groups").click()
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        # init group create
+        wd.find_element_by_name("new").click()
+        # create group
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
+        self.return_to_group_page()
+
+    def return_to_group_page(self):
+        wd = self.app.wd
+        # return_to_groups_page
+        wd.find_element_by_link_text("groups").click()
+
